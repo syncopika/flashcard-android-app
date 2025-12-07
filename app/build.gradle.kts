9736594b469ea4ff1b843d93072350e5d55c1112
nicholas hung <nhung93@outlook.com>
2025-12-06 20:01:11 -0500

    adding a view model for data fetching. the data fetching part seems to work fine but still having trouble auto-refreshing the UI after the data has been fetched. also fixed bug with shuffle (should take into account data size first).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.5.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.google.mlkit:digital-ink-recognition:18.1.0")
     implementation("androidx.compose.material3:material3-android:1.2.1")
